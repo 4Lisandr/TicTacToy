db4214a814ab6c1cda54c6367621a4cfcc4fdc8a
Alex <sumy.ua@gmail.com>
2019-09-19 15:35:47 +0300

    Very begining of application

---
+++ app/src/main/java/com/github/a4lisandr/tictactoy/TicTac.java
@@ -0,0 +1,14 @@
+package com.github.a4lisandr.tictactoy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TicTac extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tic_tac);
+    }
+}
